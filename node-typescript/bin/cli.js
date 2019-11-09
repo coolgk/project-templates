6893c482ff038f2a16dc588cec86ffeba138d2e3
Daniel Gong <coolgk@gmail.com>
2019-11-09 12:52:25 +0400

    cli update

--- node-typescript/bin/cli.js
+++ node-typescript/bin/cli.js
@@ -4,22 +4,34 @@ const fs = require('fs');
 const path = require('path');
 const { spawn } = require('child_process');
 
-const projectDirectory = createProjectDirectory(process.argv[2]);
+(async function () {
+    console.info('Initialising Project\n');
+    const projectDirectory = createProjectDirectory(process.argv[2]);
 
-initPackageJson(projectDirectory, () => installDevDependencies(projectDirectory));
+    copyFiles(projectDirectory);
 
-function runCommand(command, commandArguments, options, next) {
-    const commandProcess = spawn(command, commandArguments, options);
-    commandProcess.stdout.pipe(process.stdout);
-    commandProcess.stderr.pipe(process.stderr);
-    commandProcess.on('close', (code) => code && process.exit(code) || next && next());
+    await initPackageJson(projectDirectory);
+    console.info('Installing Dependencies\n');
+
+    await installDevDependencies(projectDirectory);
+
+    updatePackageJson(projectDirectory);
+})();
+
+function runCommand(command, commandArguments, options) {
+    return new Promise((resolve) => {
+        const commandProcess = spawn(command, commandArguments, options);
+        commandProcess.stdout.pipe(process.stdout);
+        commandProcess.stderr.pipe(process.stderr);
+        commandProcess.on('close', (code) => code && process.exit(code) || resolve());
+    });
 }
 
-function initPackageJson(cwd, next) {
-    runCommand('npm', ['init', '-y'], { cwd }, next);
+function initPackageJson(cwd) {
+    return runCommand('npm', ['init', '-y'], { cwd });
 };
 
-function installDevDependencies(cwd, next) {
+function installDevDependencies(cwd) {
     const devDependencies = [
         "@istanbuljs/nyc-config-typescript",
         "@types/chai",
@@ -42,24 +54,76 @@ function installDevDependencies(cwd, next) {
         "typescript"
     ];
 
-    runCommand(
+    return runCommand(
         'npm',
         ['i', '-D', ...devDependencies],
-        { cwd },
-        next
+        { cwd }
     );
 }
 
 function createProjectDirectory(directory = '.') {
-    const projectDirectory = path.resolve(directory);
-    fs.mkdirSync(projectDirectory, { recursive: true });
-    return projectDirectory;
-}
-
-function getProjectFolders() {
-    return [
+    const subDirectories = [
+        '',
         'src',
         'tests/src',
+        'dist',
         'tmp'
-    ];
+    ].map(subDirectory => path.resolve(directory, subDirectory));
+
+    subDirectories.forEach((subDirectory) => {
+        fs.mkdirSync(subDirectory, { recursive: true });
+    });
+
+    return subDirectories[0];
 }
+
+function copyFiles(directory) {
+    const files = [
+        ['src/index.ts'],
+        ['tests/src/index.test.ts'],
+        ['tests/setup.ts'],
+        ['.eslintignore'],
+        ['.eslintrc'],
+        ['.gitignore'],
+        ['.mocharc.json'],
+        ['.nycrc'],
+        ['nodemon.json'],
+        ['README.md'],
+        ['tsconfig.json'],
+        ['.env.example', '.env']
+    ];
+
+    files.forEach(([templateFilename, targetFilename]) => {
+        const targetFile = path.resolve(directory, targetFilename || templateFilename);
+
+        if (fs.existsSync(targetFile)) {
+            const basename = path.basename(targetFile);
+            fs.copyFileSync(targetFile, `${targetFile}.original`);
+            console.info(`renamed existing ${basename} to ${basename}.original`);
+        }
+
+        fs.copyFileSync(
+            path.resolve(__dirname, '..', 'template', templateFilename),
+            targetFile
+        );
+    });
+}
+
+function updatePackageJson(directory) {
+    const targetPackageJsonPath = path.resolve(directory, 'package.json');
+    const templatePackageJson = require('../template/package.json');
+    const targetPackageJson = require(targetPackageJsonPath);
+
+    const properties = [
+        'scripts', 'main', 'husky'
+    ];
+
+    properties.forEach((property) => {
+        if (targetPackageJson[property]) {
+            targetPackageJson[`${property}.original`] = targetPackageJson[property];
+        }
+        targetPackageJson[property] = templatePackageJson[property];
+    });
+
+    fs.writeFileSync(targetPackageJsonPath, JSON.stringify(targetPackageJson, null, 4));
+};
