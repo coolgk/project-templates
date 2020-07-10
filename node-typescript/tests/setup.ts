b5bca5920ac21041335112414c2b45c330a2ddbc
Daniel Gong <coolgk@gmail.com>
2020-07-10 12:45:06 +0100

    fix: mocha global hooks

--- node-typescript/tests/setup.ts
+++ node-typescript/tests/setup.ts
@@ -2,4 +2,13 @@
  * test setup file
  */
 
-// console.info('set up test');
+console.info("test setup file");
+
+export const mochaHooks = {
+  beforeAll(): void {
+    console.log("global before all");
+  },
+  afterAll(): void {
+    console.log("global after all");
+  },
+};
